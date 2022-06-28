4c259f9564f0b98bbea5995e584495da74ff8486
DrazhinUstin <zmagar91@gmail.com>
2022-06-28 06:33:45 +0300

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,56 @@
+import { useState, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { navLinks } from '../utils/localData';
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { items } = useSelector((state) => state.favorites);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const menu = menuRef.current;
+        const menuParent = menu.parentElement;
+        const menuLinks = menu.querySelectorAll('a');
+        if (isMenuOpen) {
+            document.body.style.overflow = 'hidden';
+            const { y } = menuParent.getBoundingClientRect();
+            menu.style.top = `${menuParent.offsetHeight + y}px`;
+            menuLinks.forEach((link, index) => {
+                link.style.animation = `surprise 0.25s ease-in-out ${0.25 + index / 5}s forwards`;
+            });
+        } else {
+            document.body.style.overflow = '';
+            menuLinks.forEach((link) => {
+                link.style.animation = '';
+            });
+        }
+    }, [isMenuOpen]);
+
     return (
         <nav className='navbar'>
             <div className='section-center'>
-                <div className='navbar-menu'>
-                    <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : null)}>
-                        home
-                    </NavLink>
-                    <NavLink
-                        to='characters'
-                        className={({ isActive }) => (isActive ? 'active' : null)}
-                    >
-                        heroes
-                    </NavLink>
-                    <NavLink to='comics' className={({ isActive }) => (isActive ? 'active' : null)}>
-                        comics
-                    </NavLink>
-                    <NavLink
-                        to='favorites'
-                        className={({ isActive }) => (isActive ? 'active' : null)}
-                    >
-                        favorites
-                    </NavLink>
+                <div className={`navbar-menu ${isMenuOpen && 'open'}`} ref={menuRef}>
+                    {navLinks.map(({ id, title, path }) => {
+                        return (
+                            <NavLink
+                                key={id}
+                                to={path}
+                                className={({ isActive }) => (isActive ? 'active' : null)}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                {title}
+                                {title === 'favorites' && <span>{items.length}</span>}
+                            </NavLink>
+                        );
+                    })}
                 </div>
+                <button
+                    className={`navbar-toggle-btn ${isMenuOpen && 'active'}`}
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                    <span></span>
+                </button>
             </div>
         </nav>
     );
---
+++ src/utils/localData.js
@@ -0,0 +1,24 @@
+const navLinks = [
+    {
+        id: 1,
+        title: 'home',
+        path: '/',
+    },
+    {
+        id: 2,
+        title: 'heroes',
+        path: 'characters',
+    },
+    {
+        id: 3,
+        title: 'comics',
+        path: 'comics',
+    },
+    {
+        id: 4,
+        title: 'favorites',
+        path: 'favorites',
+    },
+];
+
+export { navLinks };
